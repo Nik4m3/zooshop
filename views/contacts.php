663261bb916e9a9044d319677da04c52a4441bd6
Vladimir <vladimirdotk@users.noreply.github.com>
2015-10-30 22:47:55 +0500

    Update contacts.php

--- views/contacts.php
+++ views/contacts.php
@@ -2,8 +2,8 @@
 <div class="center-block text-center">
 	<img class="img-circle" src="img/vladimir.jpg">
 	<hr>
-	<p><b>Обо мне</b>: Владимир Киверник, студент ЧелГУ, ИИТ, группа ИТВЗ-201</p>
+	<p><b>Обо мне</b>: Владимир, студент ЧелГУ, ИИТ, группа ИТВЗ-301</p>
 	<p><b>Изучаю языки</b>: php, python, javascript, go</p>
-	<p><b>Email</b>: vladimir@kivernik.ru</p>
+	<p><b>Email</b>: ***</p>
 	<p><b>Jabber</b>: vladimirk@jabber.ru</p>
 </div>
